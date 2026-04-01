fd0044b8eb747dca2f23570e48065c1d32c08538
MdMoiz02 <mdmoiz2929@gmail.com>
2026-04-01 22:26:11 +0530

    program for letter template

--- python5.py
+++ python5.py
@@ -1,9 +1,5 @@
-"""Write a program to fill in a letter template given below with name and date.
-"""
+"""Write a program to fill in a letter template given below with name and date."""
 name = input("Enter your name: ")
 date = input("Enter the date: ")
-print("\n                     LETTER TEMPLATE")
-print(f"                                         {date}")
-print(f"Dear {name},")
-print(f"\nI hope this letter finds you well.") #can write any message here
-print("\nBest regards,")
+letter = f'''Dear {name}, You are selected! {date}'''
+print(letter)
