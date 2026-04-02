0c858622ee9968e5f2da17ba6a18e32c68e6d71b
MdMoiz02 <mdmoiz2929@gmail.com>
2026-04-02 22:18:44 +0530

    Add double space detection in a string
    Implement a program to detect double spaces in a string.

---
+++ python9.py
@@ -0,0 +1,7 @@
+"""Write a program to detect double space in a string.
+"""
+string = input("Enter a string: ")
+if "  " in string:
+    print("Double space found!")
+else:
+    print("Double space is not found.")
