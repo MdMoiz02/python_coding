7d36f52b325508314c2d68efbeb06e7488c82f05
MdMoiz02 <mdmoiz2929@gmail.com>
2026-04-02 22:20:43 +0530

    Add name presence check in a list program
    Implement a name presence check in a list.

---
+++ python10.py
@@ -0,0 +1,8 @@
+"""Write a program which finds out whether a given name is present in a list or not. 
+"""
+ls=["Moiz","Venu","Ram"]
+name= input("Enter name:")
+if name in ls:
+    print("Entered name is present in the list")
+else:
+    print ("Entered name is not found in list")
