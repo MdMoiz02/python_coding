be8a357b8bbffc0d09b85cfd5ac65543a09760d9
MdMoiz02 <mdmoiz2929@gmail.com>
2026-04-02 22:02:46 +0530

    Add username length validation
    Implement a program to check username length.

---
+++ python6.py
@@ -0,0 +1,7 @@
+""" Write a program to find whether a given username contains less than 10 
+characters or not."""
+string=input("Enter your string:")
+if len(string)<10:
+    print("string is less than 10 characters")
+else:
+    print ("string is more than 10")
