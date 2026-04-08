fd7a76aeb5ad89eda17fe50bf9bb382f313fb327
MdMoiz02 <mdmoiz2929@gmail.com>
2026-04-08 20:23:00 +0530

    Add program to count ones in a tuple

---
+++ python18.py
@@ -0,0 +1,4 @@
+"""Write a program to count the number of ones in the following tuple: A = (7, 1, 8, 1, 1, 9) """
+A = (7, 1, 8, 1, 1, 9)
+count = A.count(1)
+print("The number of ones in the tuple is:", count)
