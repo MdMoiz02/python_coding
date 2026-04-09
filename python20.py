daadca6421c0e8c6137a4e878fbb5eb3ba757035
MdMoiz02 <mdmoiz2929@gmail.com>
2026-04-09 17:46:33 +0530

    Sort list of tuples by last element
    Sort the list of tuples based on the last element.

---
+++ python20.py
@@ -0,0 +1,4 @@
+"""Given a list of tuples, write a program to sort the list based on the last element of each tuple."""
+list_of_tuples = [(1, 3), (2, 1), (3, 2)]  
+sorted_list = sorted(list_of_tuples)
+print(sorted_list)
